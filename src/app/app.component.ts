4feabf0533735681551a70a37f2aab248bd38d7c
Jacob Nelson <jacobnelson79@gmail.com>
2017-02-16 21:16:11 +0530

    uploading all files in src folder.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app',
-  styleUrls: ['./app.component.css'],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import { NgModule, ApplicationRef } from '@angular/core'
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
@@ -6,6 +6,7 @@ import { GithubService } from './github/shared/github.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
@@ -14,8 +15,9 @@ import { RepoListComponent } from './github/repo-list/repo-list.component';
 import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
-import { GeoChartComponent } from './geochart/geochart.component';
+import { RealtimeScaleComponent } from './realtime-scale/realtime-scale.component';
 import { D3Service } from 'd3-ng2-service';
+import * as D3 from 'd3';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,7 @@ import { D3Service } from 'd3-ng2-service';
     RepoDetailComponent,
     HomeComponent,
     ContactComponent,
-    GeoChartComponent
+    RealtimeScaleComponent
   ],
   imports: [
     BrowserModule,
@@ -36,8 +38,7 @@ import { D3Service } from 'd3-ng2-service';
     RouterModule.forRoot(rootRouterConfig, { useHash: true })
   ],
   providers: [
-    GithubService,
-    D3Service
+    GithubService
   ],
   bootstrap: [ AppComponent ]
 })
