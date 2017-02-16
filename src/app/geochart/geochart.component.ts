61db12f71665146735aa48215b6e6d87a1148aad
Dexter Antony Noronha <dexter.a.noronha@gmail.com>
2017-02-16 10:10:35 +0530

    Updating the Geo Chart View
    Completed the country mesh and added a simple mock data to show the bubbles based on the co-ordinates

--- src/app/geochart/geochart.component.ts
+++ src/app/geochart/geochart.component.ts
@@ -10,124 +10,96 @@ declare var topojson: any;
 })
 
 export class GeoChartComponent implements OnInit {
-  private dg3: D3;
-  private parentNativeElement: any;
+    private dg3: D3;
+    private parentNativeElement: any;
 
-  constructor(element: ElementRef, d3Service: D3Service) { // <-- pass the D3 Service into the constructor
-    this.dg3 = d3Service.getD3(); // <-- obtain the d3 object from the D3 Service
-    this.parentNativeElement = element.nativeElement;
+    constructor(element: ElementRef, d3Service: D3Service) { // <-- pass the D3 Service into the constructor
+        this.dg3 = d3Service.getD3(); // <-- obtain the d3 object from the D3 Service
+        this.parentNativeElement = element.nativeElement;
+    }
 
-  }
+    ngOnInit() {
+        let height=600, width=950;
 
-  // ngOnInit() {
-  //   let dg3 = this.dg3;
-  //   let d3ParentElement: Selection<any, any, any, any>;
-  //   let width = 960,
-  //   height = 600,
-  //   formatNumber = dg3.format(",.0f");
-  //   let path = dg3.geoPath(null);
-  //   let radius = dg3.scaleSqrt()
-  //       .domain([0, 1e6])
-  //       .range([0, 15]);
+        let projection = d3.geo.mercator()
+        .scale(1000)
+        .translate([width / 3, height/2.1]);
 
-  //   let svg = dg3.select("body").append("svg")
-  //       .attr("width", width)
-  //       .attr("height", height);
+        let radius = d3.scale.sqrt()
+            .domain([0, 1e6])
+            .range([0, 15]);
 
-  //   let legend = svg.append("g")
-  //       .attr("class", "legend")
-  //       .attr("transform", "translate(" + (width - 50) + "," + (height - 20) + ")")
-  //   .selectAll("g")
-  //       .data([1e6, 5e6, 1e7])
-  //   .enter().append("g");
+        let path = d3.geo.path().projection(projection);
 
-  //   legend.append("circle")
-  //       .attr("cy", function(d) { return -radius(d); })
-  //       .attr("r", radius);
+        d3.json('./app/uscoordinates.json', function(error, data){
 
-  //   legend.append("text")
-  //       .attr("y", function(d) { return -2 * radius(d); })
-  //       .attr("dy", "1.3em")
-  //       .text(dg3.format(".1s"));
+            let states = topojson.feature(data, data.objects.states).features;
 
-  //   // d3.request("../../us1.json")
-  //   // .mimeType("application/json")
-  //   // .response(function(xhr) {
-  //   //   console.log(xhr.responseText); 
-  //   //   return JSON.parse(xhr.responseText); 
-  //   // });
+            projection
+                .scale(1000)
+                .center([-106, 37.5])
+            
+            let radius = d3.scale.sqrt()
+                .domain([0, 1e6])
+                .range([0, 15]);
 
-  //   d3.json('./app/us1.json', function(error, us) {
-  //       console.log(us);
-  //   });
+            let svg = d3.select("#geo-chart").append("svg")
+                    .attr("width", width)
+                    .attr("height", height);
 
-  //   dg3.select(self.frameElement).style("height", height + "px");
+            let legend = svg.append("g")
+                .attr("class", "legend")
+                .attr("transform", "translate(" + (width - 50) + "," + (height - 20) + ")")
+                .selectAll("g")
+                .data([1e6, 5e6, 1e7])
+                .enter().append("g");
 
-  // }
+            let aa = {
+                "coords": [-84.044998, 33.804443],
+                "properties": {
+                        "hub_name":"G5SNELLVILLE (GA)",
+                        "population": 1153795
+                    },
+            };
 
-  ngOnInit () {
-    let width = 960,
-    height = 600;
+            legend.append("circle")
+                .attr("cy", function(d) { return -radius(d); })
+                .attr("r", radius);
 
-    let formatNumber = d3.format(",.0f");
+            legend.append("text")
+                .attr("y", function(d) { return -2 * radius(d); })
+                .attr("dy", "1.3em")
+                .text(d3.format(".1s"));
+            
+            svg.selectAll("path")
+                .data(states).enter()
+                .append("path")
+                .attr("class", "border border--state land")
+                .attr("d", path);
 
-    let path = d3.geo.path()
-        .projection(null);
+            svg.append("path")
+                .datum(topojson.mesh(data, data.objects.states, function(a, b) { return a !== b; }))
+                .attr("class", "mesh")
+                .attr("transform", "translate(" + (width - 50) + "," + (height - 20) + ")") 
+                .attr("d", path);
 
-    let radius = d3.scale.sqrt()
-        .domain([0, 1e6])
-        .range([0, 15]);
-
-    let svg = d3.select("#geo-chart").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    let legend = svg.append("g")
-        .attr("class", "legend")
-        .attr("transform", "translate(" + (width - 50) + "," + (height - 20) + ")")
-    .selectAll("g")
-        .data([1e6, 5e6, 1e7])
-    .enter().append("g");
-
-    legend.append("circle")
-        .attr("cy", function(d) { return -radius(d); })
-        .attr("r", radius);
-
-    legend.append("text")
-        .attr("y", function(d) { return -2 * radius(d); })
-        .attr("dy", "1.3em")
-        .text(d3.format(".1s"));
-// ===== DATA JSON ADDED HERE =====//
-    d3.json("./app/us1.json", function(error, us) {
-        console.log(us);
-    if (error) throw error;
-
-    svg.append("path")
-        .datum(topojson.feature(us, us.objects.nation))
-        .attr("class", "land")
-        .attr("d", path);
-
-    svg.append("path")
-        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-        .attr("class", "border border--state")
-        .attr("d", path);
-
-    svg.append("g")
-        .attr("class", "bubble")
-        .selectAll("circle")
-        .data(topojson.feature(us, us.objects.counties).features
-            .sort(function(a, b) { return b.properties.population - a.properties.population; }))
-        .enter().append("circle")
-        .attr("transform", function(d) { return "translate(" + path.centroid(d) + ")"; })
-        .attr("r", function(d) { return radius(d.properties.population); })
-        .append("title")
-        .text(function(d) {
-            return d.properties.name
-                + "\nPopulation " + formatNumber(d.properties.population);
+            svg.append("g")
+                .attr("class", "bubble")
+                .selectAll("circle")
+                .data([aa]).enter()
+                .append("circle")
+                .attr("cx", function (d) { return projection(d.coords)[0]; })
+                .attr("cy", function (d) { return projection(d.coords)[1];})
+                .attr("r", function(d) { return radius(d.properties.population); })
+                .attr("fill", "lightred").on('mouseover', function(d){
+                    var name = d.properties.hub_name;
+                    //==== TOOL TIP SHOULD COME HERE ====
+                    console.log(name);
+                    //return document.getElementById('name').innerHTML=name;
+                })
         });
-    });
-
-    d3.select(self.frameElement).style("height", height + "px");
-  }
+        d3.select(self.frameElement).style("height", height + "px")
+            .style("width", width + "px");
+    }
 
 }
