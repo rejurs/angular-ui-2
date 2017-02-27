cb64f34f00af2258753245b84fa6b4adad360b89
Shafeek SM <shafeeksmsct@gmail.com>
2017-02-27 19:37:02 +0530

    Real-time socket data structure update

--- src/app/services/geoservice.ts
+++ src/app/services/geoservice.ts
@@ -15,7 +15,7 @@ import { WebSocketService } from '../services/socketservice';
 @Injectable()
 export class GeoDataService {
 
-    private geoDataDetails : any;
+    private geoDataDetails: any;
     private usCoordinates: any;
     private overallCountData: number;
     private hubDetails: HubNames[];
@@ -29,15 +29,16 @@ export class GeoDataService {
     marketData: Subject<MarketMetaDataModel[]> = new Subject<MarketMetaDataModel[]>();
     errorMetaData: Subject<ErrorMetaDataModel[]> = new Subject<ErrorMetaDataModel[]>();
     divisionMetaData: Subject<DivisionMetaDataModel[]> = new Subject<DivisionMetaDataModel[]>();
+    realTimeSocketData: Subject<any[]> = new Subject<any[]>();
 
-    constructor(private http: Http, private _socketservice: WebSocketService ) {
+    constructor(private http: Http, private _socketservice: WebSocketService) {
         console.log('Geo Service created...');
     }
 
     getHistoricalData() {
         let that = this;
         return Observable.create(function (observer) {
-            that._socketservice.historicalData.subscribe( (data) => {
+            that._socketservice.historicalData.subscribe((data) => {
                 that.geoDataDetails = data;
                 that.overallCountData = data['overallCount'];
                 that.errorMeta = data['countByErrorCode'];
@@ -50,196 +51,57 @@ export class GeoDataService {
     }
 
     /** To increase the KPI error code count */
-    errorCodeIncrement( code: string ) {
+    errorCodeIncrement(code: string) {
         this.errorMeta.forEach(errorElem => {
-            if(errorElem.name === code) {
+            if (errorElem.name === code) {
                 errorElem.count += 1;
             }
         });
     }
 
-    getGeoData () {
-        if(this.geoDataDetails) {
+    getGeoData() {
+        if (this.geoDataDetails) {
             console.log("here the call....");
             return Observable.of(this.geoDataDetails);
         } else {
             return this.http.get('./app/geodata/geodatamock.json')
-                .map((res:Response) => {
-                return res.json()
-            })
-            .do((data => {
-                this.geoDataDetails = data;
-                this.overallCountData = data['overallCount'];
-                this.errorMeta = data['countByErrorCode'];
-                this.marketMeta = data['countByMarket'];
-                this.divisionMeta = data['countByDivision'];
-                Observable.of(this.hubDetails = data['hub']);
-            }));
+                .map((res: Response) => {
+                    return res.json()
+                })
+                .do((data => {
+                    this.geoDataDetails = data;
+                    this.overallCountData = data['overallCount'];
+                    this.errorMeta = data['countByErrorCode'];
+                    this.marketMeta = data['countByMarket'];
+                    this.divisionMeta = data['countByDivision'];
+                    Observable.of(this.hubDetails = data['hub']);
+                }));
         }
     }
-    
+
     ngOnInit() {
         let that = this;
-        this._socketservice.realTimeData.subscribe( (data) => {
-            this.hubDetails.forEach(element => {
-                if(element.name == data['HubName']) {
-                    element.total = String(Number(element.total) + 1);
-                    element.isNew = true;
-                    element.uid = this.slugify(element.name);
-                    //Check to count outsideheadend, withinheadend, fibernodeissue and KPI error counts
-                    if(data.hasOwnProperty('1600112')) {
-                        element.outsideHeadEnd += 1;
-                        that.errorCodeIncrement('1600112');
-                    } else if(data.hasOwnProperty('1600117')) { 
-                        element.withinHeadEnd += 1;   // Sum of 1600117 + 1600119
-                        that.errorCodeIncrement('1600117');
-                    } else if(data.hasOwnProperty('1600118')) { 
-                        element.fiberNodeIssue += 1;  // Sum of 1600118 + 1600119
-                        element.outsideHeadEnd += 1;  // Sum of 1600118 + 1600112
-                        that.errorCodeIncrement('1600118');
-                    } else if(data.hasOwnProperty('1600119')) {
-                        element.fiberNodeIssue += 1;  // Sum of 1600118 + 1600119
-                        that.errorCodeIncrement('1600119');
-                    }
+        this._socketservice.realTimeData.subscribe((data) => {
 
-                    //check to increment the market count
-                    that.incrementMarketCount(element.market);
+            data.forEach(item => {
 
-                    //check to increment the division count
-                    that.incrementDivisionCount(element.division);
-                }
-            });
-            this.socketData.next(this.hubDetails);
-            this.overallCountData += 1; 
-            this.overallCount.next( new OverallMetaModel( this.overallCountData ) ) ;
-            this.errorMetaData.next(this.errorMeta);
-            this.marketData.next(this.marketMeta);
-            this.divisionMetaData.next(this.divisionMeta);
-        });
-    }
-
-    generateUsCoordinates () {
-        return this.http.get('./app/uscoordinates.json')
-            .map((res: Response) => {
-            return res.json();
-        })
-        .do((data => {
-            this.usCoordinates = data;
-            this.socketData.next(this.hubDetails);
-            this.overallCount.next(new OverallMetaModel(this.overallCountData));
-            this.errorMetaData.next(this.errorMeta);
-            this.marketData.next(this.marketMeta);
-            this.divisionMetaData.next(this.divisionMeta);
-        }));
-    }
-
-    getHubData () {
-        return Observable.of(this.hubDetails).publish().refCount();
-    }
-
-    getGeoViewData () {
-        return Observable.of(this.geoDataDetails).publish().refCount();
-    }
-
-    getUsCoordinates () {
-        return Observable.of(this.usCoordinates).publish().refCount();
-    }
-
-    generateSocketData() {
-        /** Logic to add the data */
-        let hubs = [{
-            HubName: 'OREM (UT)',
-            1600112: 1
-        }, {
-            HubName: 'SOUTH (IN)',
-            1600117: 1
-        },{
-            Hubname: 'POTOMAC (MD)',
-            1600118: 1
-        },{
-            HubName:'JEFFERSON (PA)',
-            1600119: 1
-        },{
-            HubName: 'RURAL VALLEY (PA)',
-            1600112: 1
-        },{
-            HubName: 'Y2CARTERSVILLE (GA)',
-            1600113: 1
-        }, {
-            Hubname:'F3SFULTON (GA)',
-            1600119:1
-        }, {
-            HubName: 'PLAINFIELD (NJ)',
-            1600118:1
-        }, {
-            HubName: 'WARREN 1-2 (MI)',
-            1600117: 1
-        }, {
-            HubName:'WINCHESTER CITY (TN)',
-            1600112: 1
-        }, {
-            HubName: 'HIALEAH WEST (FL)',
-            1600118: 1
-        }, {
-            HubName: 'ST JOSEPH (MI)',
-            1600117: 1
-        }, {
-            HubName: 'ABERDEEN (MD)',
-            1600112: 1
-        }, {
-            HubName: 'MANASSAS (VA)',
-            1600118: 1
-        }, {
-            HubName: 'WATERBURY (CT)',
-            1600119: 1
-        }, {
-            HubName: 'WEST YORK (PA)',
-            1600118: 1
-        }, {
-            HubName: 'FOREST HILL (VA)',
-            1600119: 1
-        }, {
-            HubName: 'SNYDER (AZ)',
-            1600119: 1
-        }, {
-            HubName: 'HAMMOND (IN)',
-            1600112: 1
-        }, {
-            HubName: 'KEY LARGO (FL)',
-            1600117: 1
-        }, {
-            HubName: 'CORVALLIS (OR)',
-            1600112: 1
-        }, {
-            HubName: 'WEST CHICAGO-POD-6 (IL)',
-            1600118: 1
-        }, {
-            HubName: 'COLUMBUS (PA)',
-            1600117: 1
-        }];
-
-        return Observable.of(setInterval( () => {
-            let that = this;
-            var data: any;
-            if(Math.random() > 0.85) {
-                data = hubs[Math.floor(Math.random()*hubs.length)];
                 this.hubDetails.forEach(element => {
-                    if(element.name == data['HubName']) {
+                    if (element.name == item['HubName']) {
                         element.total = String(Number(element.total) + 1);
                         element.isNew = true;
                         element.uid = this.slugify(element.name);
                         //Check to count outsideheadend, withinheadend, fibernodeissue and KPI error counts
-                        if(data.hasOwnProperty('1600112')) {
+                        if (item.hasOwnProperty('1600112')) {
                             element.outsideHeadEnd += 1;
                             that.errorCodeIncrement('1600112');
-                        } else if(data.hasOwnProperty('1600117')) { 
+                        } else if (item.hasOwnProperty('1600117')) {
                             element.withinHeadEnd += 1;   // Sum of 1600117 + 1600119
                             that.errorCodeIncrement('1600117');
-                        } else if(data.hasOwnProperty('1600118')) { 
+                        } else if (item.hasOwnProperty('1600118')) {
                             element.fiberNodeIssue += 1;  // Sum of 1600118 + 1600119
                             element.outsideHeadEnd += 1;  // Sum of 1600118 + 1600112
                             that.errorCodeIncrement('1600118');
-                        } else if(data.hasOwnProperty('1600119')) {
+                        } else if (item.hasOwnProperty('1600119')) {
                             element.fiberNodeIssue += 1;  // Sum of 1600118 + 1600119
                             that.errorCodeIncrement('1600119');
                         }
@@ -251,34 +113,190 @@ export class GeoDataService {
                         that.incrementDivisionCount(element.division);
                     }
                 });
+            });
+
+            this.socketData.next(this.hubDetails);
+            this.overallCountData += 1;
+            this.overallCount.next(new OverallMetaModel(this.overallCountData));
+            this.errorMetaData.next(this.errorMeta);
+            this.marketData.next(this.marketMeta);
+            this.divisionMetaData.next(this.divisionMeta);
+            this.realTimeSocketData.next(data);
+        });
+    }
+
+    generateUsCoordinates() {
+        return this.http.get('./app/uscoordinates.json')
+            .map((res: Response) => {
+                return res.json();
+            })
+            .do((data => {
+                this.usCoordinates = data;
                 this.socketData.next(this.hubDetails);
-                this.overallCountData += 1; 
-                this.overallCount.next( new OverallMetaModel( this.overallCountData ) ) ;
+                this.overallCount.next(new OverallMetaModel(this.overallCountData));
                 this.errorMetaData.next(this.errorMeta);
                 this.marketData.next(this.marketMeta);
                 this.divisionMetaData.next(this.divisionMeta);
+            }));
+    }
+
+    getHubData() {
+        return Observable.of(this.hubDetails).publish().refCount();
+    }
+
+    getGeoViewData() {
+        return Observable.of(this.geoDataDetails).publish().refCount();
+    }
+
+    getUsCoordinates() {
+        return Observable.of(this.usCoordinates).publish().refCount();
+    }
+
+    generateSocketData() {
+        /** Logic to add the data */
+        let hubs = [{
+            HubName: 'OREM (UT)',
+            1600112: 1
+        }, {
+                HubName: 'SOUTH (IN)',
+                1600117: 1
+            }, {
+                Hubname: 'POTOMAC (MD)',
+                1600118: 1
+            }, {
+                HubName: 'JEFFERSON (PA)',
+                1600119: 1
+            }, {
+                HubName: 'RURAL VALLEY (PA)',
+                1600112: 1
+            }, {
+                HubName: 'Y2CARTERSVILLE (GA)',
+                1600113: 1
+            }, {
+                Hubname: 'F3SFULTON (GA)',
+                1600119: 1
+            }, {
+                HubName: 'PLAINFIELD (NJ)',
+                1600118: 1
+            }, {
+                HubName: 'WARREN 1-2 (MI)',
+                1600117: 1
+            }, {
+                HubName: 'WINCHESTER CITY (TN)',
+                1600112: 1
+            }, {
+                HubName: 'HIALEAH WEST (FL)',
+                1600118: 1
+            }, {
+                HubName: 'ST JOSEPH (MI)',
+                1600117: 1
+            }, {
+                HubName: 'ABERDEEN (MD)',
+                1600112: 1
+            }, {
+                HubName: 'MANASSAS (VA)',
+                1600118: 1
+            }, {
+                HubName: 'WATERBURY (CT)',
+                1600119: 1
+            }, {
+                HubName: 'WEST YORK (PA)',
+                1600118: 1
+            }, {
+                HubName: 'FOREST HILL (VA)',
+                1600119: 1
+            }, {
+                HubName: 'SNYDER (AZ)',
+                1600119: 1
+            }, {
+                HubName: 'HAMMOND (IN)',
+                1600112: 1
+            }, {
+                HubName: 'KEY LARGO (FL)',
+                1600117: 1
+            }, {
+                HubName: 'CORVALLIS (OR)',
+                1600112: 1
+            }, {
+                HubName: 'WEST CHICAGO-POD-6 (IL)',
+                1600118: 1
+            }, {
+                HubName: 'COLUMBUS (PA)',
+                1600117: 1
+            }];
+
+        return Observable.of(setInterval(() => {
+            let that = this;
+            var data: any;
+
+            if (Math.random() > 0.85) {
+
+                data = hubs[Math.floor(Math.random() * hubs.length)];
+                data.time = +new Date();
+                
+                let realData: any[] = [data];
+                
+                realData.forEach(item => {
+
+                    this.hubDetails.forEach(element => {
+                        if (element.name == item['HubName']) {
+                            element.total = String(Number(element.total) + 1);
+                            element.isNew = true;
+                            element.uid = this.slugify(element.name);
+                            //Check to count outsideheadend, withinheadend, fibernodeissue and KPI error counts
+                            if (item.hasOwnProperty('1600112')) {
+                                element.outsideHeadEnd += 1;
+                                that.errorCodeIncrement('1600112');
+                            } else if (item.hasOwnProperty('1600117')) {
+                                element.withinHeadEnd += 1;   // Sum of 1600117 + 1600119
+                                that.errorCodeIncrement('1600117');
+                            } else if (item.hasOwnProperty('1600118')) {
+                                element.fiberNodeIssue += 1;  // Sum of 1600118 + 1600119
+                                element.outsideHeadEnd += 1;  // Sum of 1600118 + 1600112
+                                that.errorCodeIncrement('1600118');
+                            } else if (item.hasOwnProperty('1600119')) {
+                                element.fiberNodeIssue += 1;  // Sum of 1600118 + 1600119
+                                that.errorCodeIncrement('1600119');
+                            }
+
+                            //check to increment the market count
+                            that.incrementMarketCount(element.market);
+
+                            //check to increment the division count
+                            that.incrementDivisionCount(element.division);
+                        }
+                    });
+                });
+
+                this.socketData.next(this.hubDetails);
+                this.overallCountData += 1;
+                this.overallCount.next(new OverallMetaModel(this.overallCountData));
+                this.errorMetaData.next(this.errorMeta);
+                this.marketData.next(this.marketMeta);
+                this.divisionMetaData.next(this.divisionMeta);
+                this.realTimeSocketData.next(realData);
             }
         }, 1000))
     }
 
     /** To increase the KPI Market count */
-    incrementMarketCount(marketName: string ) {
-        this.marketMeta.forEach( marketItem => {
-            if(marketItem.name === marketName) {
+    incrementMarketCount(marketName: string) {
+        this.marketMeta.forEach(marketItem => {
+            if (marketItem.name === marketName) {
                 marketItem.count += 1;
             }
         });
     }
 
-    incrementDivisionCount ( divsionName: string ) {
-        this.divisionMeta.forEach( divisionItem => {
-            if(divisionItem.name === divsionName) {
+    incrementDivisionCount(divsionName: string) {
+        this.divisionMeta.forEach(divisionItem => {
+            if (divisionItem.name === divsionName) {
                 divisionItem.count += 1;
             }
-        } );
+        });
     }
 
-    slugify (arg: string) {
+    slugify(arg: string) {
         return arg.toString().toLowerCase()
             .replace(/\s+/g, '-')           // Replace spaces with -
             .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
@@ -287,8 +305,8 @@ export class GeoDataService {
             .replace(/-+$/, '');            // Trim - from end of text
     }
 
-    countOutsideHeadend (element) {
-        
+    countOutsideHeadend(element) {
+
     }
 
 }
