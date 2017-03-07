eb3947277748d52d52f2d5e142533c2c41702db6
Thapanchand <thapanchand@gmail.com>
2017-03-07 13:02:06 +0530

    Changes made to solve the undefined count in sort

--- src/app/geometadata/divisionmetadata/divisionmetadata.component.ts
+++ src/app/geometadata/divisionmetadata/divisionmetadata.component.ts
@@ -26,7 +26,7 @@ export class DivisionMetaDataComponent {
         divisionData.sort(function(a, b) {
             a.name = _.capitalize(a.name);
             b.name = _.capitalize(b.name);
-            return b.count - a.count;
+            return _.get(b, 'count', 0) - _.get(a, 'count', 0);
         });
 
         let divisionItems = [];
@@ -35,19 +35,19 @@ export class DivisionMetaDataComponent {
             case 'geo-central-division' :
                 divisionItems.push({
                     name: "Total Counts",
-                    count: divisionData[0].count
+                    count: _.get(divisionData,'[0].count', 0)
                 });
                 break;
             case 'geo-ne-division' :
                 divisionItems.push({
                     name: "Total Counts",
-                    count: divisionData[1].count
+                    count: _.get(divisionData,'[1].count', 0)
                 }); 
                 break;
             case 'geo-west-division' :
                 divisionItems.push({
                     name: "Total Counts",
-                    count: divisionData[2].count
+                    count: _.get(divisionData,'[2].count', 0)
                 }); 
                 break;
             case 'kpi' :
