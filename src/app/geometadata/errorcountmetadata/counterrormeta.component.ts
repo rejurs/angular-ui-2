fe7a54795591aba57095ce2b77c6b3e59a43e659
Thapanchand <thapanchand@gmail.com>
2017-03-08 15:38:47 +0530

    Changes made to change the Mismatch conditions in KPI

--- src/app/geometadata/errorcountmetadata/counterrormeta.component.ts
+++ src/app/geometadata/errorcountmetadata/counterrormeta.component.ts
@@ -11,11 +11,24 @@ export class CountErrorMetaComponent {
 
     errorCodes: ErrorMetaDataModel[]
 
-    constructor (private _geoservice : GeoDataService) {}
+    codeNames: any[] = [];
+
+    constructor (private _geoservice : GeoDataService) {
+
+        this.codeNames['1600112'] = '1600112 - Single CMTS comparison - CMTS Mismatch, Headend Mismatch';
+        this.codeNames['1600117'] = '1600117 - Single CMTS comparison - CMTS Mismatch, Headend Match';
+        this.codeNames['1600118'] = '1600118 - Multiple CMTS comparison - CMTS Mismatch, Headend Mismatch';
+        this.codeNames['1600119'] = '1600119 - Multiple CMTS comparison  - CMTS Mismatch, Headend Match';
+    }
 
     ngOnInit() {
         this._geoservice.errorMetaData.subscribe( (errorData) => {
+            // console.log(errorData)
             this.errorCodes = errorData;
         })
+    }
+
+    getName(key: string) : string {
+        return this.codeNames[key];
     }
 }
